38e9574e46e60a4594431424f2d587d382d4496b
davidmikulic03 <d.mikulic03@gmail.com>
2024-10-03 16:42:32 +0200

    feat: started work on C++ side of AI memory

---
+++ Source/gp4_team_01/AI/WeightedSignal.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include "CoreMinimal.h"
+#include "gp4_team_01/Enemies/PerceptionSignal.h"
+
+#include "WeightedSignal.generated.h"
+
+class AEnemyBase;
+
+USTRUCT(BlueprintType)
+struct FWeightedSignal {
+	GENERATED_BODY()
+    
+	FWeightedSignal() { }
+	FWeightedSignal(FPerceptionSignal Signal, float Weight)
+		: Signal(Signal), Weight(Weight) { }
+	FWeightedSignal(FPerceptionSignal Signal)
+		: Signal(Signal) { }
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+		FPerceptionSignal Signal;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+		float Weight = 0.f;
+
+	double Time;
+};
