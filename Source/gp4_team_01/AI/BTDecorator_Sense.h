ebdf76d5ffaa96e6b38620314ebb897fa82e5243
davidmikulic03 <d.mikulic03@gmail.com>
2024-10-04 11:58:27 +0200

    tech: progress on decorator

---
+++ Source/gp4_team_01/AI/BTDecorator_Sense.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Decorators/BTDecorator_Blackboard.h"
+#include "BTDecorator_Sense.generated.h"
+
+UCLASS(BlueprintType)
+class GP4_TEAM_01_API UBTDecorator_Sense : public UBTDecorator_Blackboard
+{
+	GENERATED_BODY()
+
+protected:
+	virtual bool CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const override;
+};
